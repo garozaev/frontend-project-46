f93091494463d1d376696d4ff0be7b206f6cab6c
Georgy Rozaev <g.a.rozaev@gmail.com>
2025-01-30 22:15:40 +0300

    modified src/cli.js add arguments filepath1, filepath2 and option format

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,10 @@ const diff = () => {
   program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .argument('<filepath1>', 'path to file')
+    .argument('<filepath2>', 'path to file')
+    .option('-f, --format[type]', 'output format')
 
   program.parse();
 };
