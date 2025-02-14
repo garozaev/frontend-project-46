afcb45f222c5b794d374ef39732d231c46bd245a
Georgy Rozaev <g.a.rozaev@gmail.com>
2025-02-14 20:09:26 +0300

    step 4 without asciinema

--- src/utils.js
+++ src/utils.js
@@ -26,29 +26,26 @@ const compareFileJSON = (data1, data2) => {
   const sortData = _.sortBy(consolidatedData);
   const reduceData = sortData.reduce((acc, key) => {
     if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
-      return { ...acc, [`- ${key}`]: data1[key] };
+      return { ...acc, [`  - ${key}`]: data1[key] };
     }
     if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
-      return { ...acc, [`+ ${key}`]: data2[key] };
+      return { ...acc, [`  + ${key}`]: data2[key] };
     }
     if (data1[key] !== data2[key]) {
-      return { ...acc, [`- ${key}`]: data1[key], [`+ ${key}`]: data2[key] };
+      return { ...acc, [`  - ${key}`]: data1[key], [`  + ${key}`]: data2[key] };
     }
     if (data1[key] === data2[key]) {
-      return { ...acc, [`  ${key}`]: data1[key] };
+      return { ...acc, [`    ${key}`]: data1[key] };
     }
-    // if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
-    //   return { ...acc, ...data1 };
-    // }
-    console.log(`acc: ${acc}`);
     return { ...acc, ...data1, ...data2 };
   }, {});
+  const dataWithString = Object.entries(reduceData).map(item => item.join(' ,').replace(/,/g, ': '));
+  const stringData = `{\n${dataWithString.join().replace(/,/g, ' \n')}\n}`;
   console.log(`consolidatedData: ${consolidatedData}`);
   console.log(`sortData: ${sortData}`);
   console.log(`reduceData: ${reduceData}`);
-  return reduceData;
+  console.log(`stringData: ${stringData}`);
+  return stringData;
 };
 
 export { readFile, compareFileJSON };
-
-// Object.hasOwn(data1, key) && Object.hasOwn(data2, key)
